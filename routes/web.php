1dd08bd723f63299624dc1b3d945f255e7894e3a
kazukitooyama1998-svg <kazukitooyama1998@gmail.com>
2026-08-02 22:04:15 +0900

    Create routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\GrammarController;
+use App\Http\Controllers\StudyLogController;
+use App\Http\Controllers\VocabularyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// トップページ（すべての機能を1画面に集約）。デフォルトタブの単語・フレーズ一覧をそのまま表示する。
+Route::get('/', [VocabularyController::class, 'index'])->name('vocabularies.index');
+
+Route::resource('vocabularies', VocabularyController::class)->only(['store', 'update', 'destroy']);
+Route::resource('grammars', GrammarController::class)->only(['index', 'store', 'update', 'destroy']);
+Route::resource('study-logs', StudyLogController::class)->only(['index', 'store']);
